94c93b4ebcd5f78eb984940f536bf99300b0a8e4
NiRuaG <gaurin1107@gmail.com>
2018-12-04 23:47:38 -0600

    random mock friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,10 @@
-module.exports = {
-  name: "Ahmed",
-  photo:
-    "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-};
+const NAMES = [ "Moshe", "Charlie", "Seth", "Aleena", "Kiara", "Isabelle"];
+const PHOTO_SIZEs  = [200,250,300];
+
+module.exports = [...Array(4)].map(_=> {
+  return {
+    name  : NAMES[Math.floor(Math.random()*NAMES.length)],
+    photo : `https://fillmurray.com/${PHOTO_SIZEs[Math.floor(Math.random()*PHOTO_SIZEs.length)]}/${PHOTO_SIZEs[Math.floor(Math.random()*PHOTO_SIZEs.length)]}`,
+    scores: [...Array(10)].map(_=>Math.floor(Math.random()*5)+1),
+  }
+});
