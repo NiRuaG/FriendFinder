9ff79b3ca01adb3f13dd3319fd80a8b142aa0b73
Andrew Gaurin <gaurin1107@gmail.com>
2018-12-06 13:12:39 -0600

    best friend logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 const friendData = require("../data/friends");
 console.log(friendData);
 
+
 module.exports = (app) => {
   app.get("/api/friends", (req, res) => {
     return res.send(friendData);
@@ -8,7 +9,30 @@ module.exports = (app) => {
 
   app.post("/api/friends", (req, res) => {
     console.log(req.body);
+
+    const submittedScores = req.body.scores;
+
+    const bestFriend = friendData.reduce( (bestSoFar, currFriendCheck) => {
+
+      const currCompat = currFriendCheck.scores.reduce( (accComp, currQScore, index) => 
+        accComp + Math.abs(currQScore - submittedScores[index]), 0);
+      console.log(currCompat);
+
+      if (currCompat < bestSoFar.compatScore) {
+        return {
+          friendObj  : currFriendCheck,
+          compatScore: currCompat
+        }
+      }
+      // else 
+        return bestSoFar;
+    }, {compatScore: Infinity}); // default is most-incompatible
+
+    friendData.push(req.body);
+
+    let { friendObj: {name, photo}, compatScore } = bestFriend;
+    res.json( { name, photo, compatScore } );
     
-    res.send("add friend by survey")
+    // res.send("add friend by survey");
   });
 };
