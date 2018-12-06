d06b486fce5f71afa0a693861454ceae0ff9643d
NiRuaG <gaurin1107@gmail.com>
2018-12-06 17:46:16 -0600

    modal!

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,8 +3,8 @@ const PHOTO_SIZEs  = [200,250,300];
 
 module.exports = [...Array(4)].map( (_, index) => {
   return {
-    name  : NAMES[index],
-    photo : `https://fillmurray.com/${PHOTO_SIZEs[Math.floor(Math.random()*PHOTO_SIZEs.length)]}/${PHOTO_SIZEs[Math.floor(Math.random()*PHOTO_SIZEs.length)]}`,
-    scores: [...Array(10)].map(_ => Math.floor(Math.random()*5)+1),
+    name    : NAMES[index],
+    photoURL: `https://fillmurray.com/${PHOTO_SIZEs[Math.floor(Math.random()*PHOTO_SIZEs.length)]}/${PHOTO_SIZEs[Math.floor(Math.random()*PHOTO_SIZEs.length)]}`,
+    scores  : [...Array(10)].map(_ => Math.floor(Math.random()*5)+1),
   }
 });
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,49 +1,54 @@
-const friendData = require("../data/friends");
-console.log(friendData);
+const friendsData = require("../data/friends");
+console.log(friendsData);
 
 
 module.exports = (app) => {
   app.get("/api/friends", (req, res) => {
-    return res.send(friendData);
+    return res.send(friendsData);
   });
 
-  app.post("/api/friends", ( { body: submitObj }, res) => {
+  app.post("/api/friends", ( {body: submitObj}, res) => {
     console.log(submitObj);
 
-    if (Array.isArray(friendData) && friendData.length === 0) {
+    if (Array.isArray(friendsData) && friendsData.length === 0) {
       return res.status(500).end();
     }
 
-    const submittedScores = submitObj.scores;    
+    const submittedScores = submitObj.scores;
 
     if (!submittedScores.every(Number)) {
       return res.status(500).end();
     }
 
-    const bestFriend = friendData.reduce( (bestSoFar, currFriendCheck) => {
+    const bestFriend = friendsData.reduce( (acc_bestFriendSoFar, curr_friendToCheck) => {
 
-      const currCompat = currFriendCheck.scores.reduce( (accComp, currQScore, index) => 
-        accComp + Math.abs(currQScore - submittedScores[index]), 0);
-      console.log(currCompat);
+      const curr_friendsCompat = curr_friendToCheck.scores.reduce( (acc_compatibility, curr_QScore, index) => 
+        acc_compatibility + Math.abs(curr_QScore - submittedScores[index]), 0);
+      console.log(curr_friendsCompat);
 
-      if (Number.isNaN(currCompat)) {
-        return 
-      }
-
-      if (currCompat < bestSoFar.compatScore) {
+      if (curr_friendsCompat < acc_bestFriendSoFar.compatibility) {
         return {
-          friendObj  : currFriendCheck,
-          compatScore: currCompat
+          friendObj    : curr_friendToCheck,
+          compatibility: curr_friendsCompat
         }
       }
       // else 
-        return bestSoFar;
-    }, {compatScore: Infinity}); // default is 'most incompatible'
+        return acc_bestFriendSoFar;
+    }, {compatibility: Infinity}); // default is 'most incompatible'
 
-    friendData.push(submitObj);
+    friendsData.push(submitObj);
 
-    let { friendObj: {name, photo}, compatScore } = bestFriend;
-    res.json( { name, photo, compatScore } );
+    let { friendObj: {name, photoURL}, compatibility } = bestFriend;
+    const MAX_DIFF_PER_Q = (5-1);
+    const NUM_Qs = 10;
+    const MAX_DIFF = MAX_DIFF_PER_Q * NUM_Qs;
+    return res.json( 
+      {
+        name,
+        photoURL,
+        score: Number( (MAX_DIFF - compatibility)/(MAX_DIFF)+'e2' ).toFixed()
+      }
+    );
     
     // res.send("add friend by survey");
   });
