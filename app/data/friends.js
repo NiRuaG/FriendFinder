3bbd59eab31d188c08d893d28fce3211c4c9641b
Andrew Gaurin <gaurin1107@gmail.com>
2018-12-06 13:11:25 -0600

    not random names from list (avoid duplicates)

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,10 @@
 const NAMES = [ "Moshe", "Charlie", "Seth", "Aleena", "Kiara", "Isabelle"];
 const PHOTO_SIZEs  = [200,250,300];
 
-module.exports = [...Array(4)].map(_=> {
+module.exports = [...Array(4)].map( (_, index) => {
   return {
-    name  : NAMES[Math.floor(Math.random()*NAMES.length)],
+    name  : NAMES[index],
     photo : `https://fillmurray.com/${PHOTO_SIZEs[Math.floor(Math.random()*PHOTO_SIZEs.length)]}/${PHOTO_SIZEs[Math.floor(Math.random()*PHOTO_SIZEs.length)]}`,
-    scores: [...Array(10)].map(_=>Math.floor(Math.random()*5)+1),
+    scores: [...Array(10)].map(_ => Math.floor(Math.random()*5)+1),
   }
 });
